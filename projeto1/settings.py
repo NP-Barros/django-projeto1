40d1344329247979e9975b4af8367fe58304bdbe
DeskVitor <vitor_200@hotmail.com>
2023-01-19 01:54:02 -0300

    19/01/madrugada

--- projeto1/settings.py
+++ projeto1/settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'  # pasta de arquivos estáticos do projeto
+
+STATICFILES_DIRS = [            # este comando aponta para arquivos estáticos
+    BASE_DIR / 'base_static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from recipes.views import home
+from . import views  # O ponto é para falar que está na mesma pasta
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe),
 ]
