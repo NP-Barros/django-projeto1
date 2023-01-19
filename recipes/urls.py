89591d6aba65b6d84ece7f00782901e2e1034897
NP-Barros <vitor_200@hotmail.com>
2023-01-19 14:06:19 -0300

    sei la

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from recipes.views import home
+
 from . import views  # O ponto é para falar que está na mesma pasta
 
 urlpatterns = [
+    path('', home),
     path('', views.home),
     path('recipes/<int:id>/', views.recipe),
 ]
