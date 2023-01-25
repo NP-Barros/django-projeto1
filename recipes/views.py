79a340bca421f2e11873943aef942770edf1f78e
NP-Barros <vitor_200@hotmail.com>
2023-01-25 10:52:06 -0300

    aula 58

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
+# from recipes.models import Recipe
 from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 
 def home(request):
+    # recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': [make_recipe() for _ in range(10)],
     })
